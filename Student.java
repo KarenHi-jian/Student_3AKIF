a6c846341ef056ddd9ef28a1507584423d99ac41
karen Hi-Jian Garcia Pinal <gar240730@spengergasse.at>
2024-10-05 15:37:37 +0200

    Add some coments

--- Student.java
+++ Student.java
@@ -1,8 +1,8 @@
 public class Student
 {
-    private String name;
-    private int alter;
-    private String religion;
+    private String name;   // Name des Studierende
+    private int alter;       // Alter des Studierende
+    private String religion; //....
     private boolean matura;
     
     public Student(String neuName,int neuAlter,String neuReligion,boolean neuMatura)
@@ -16,6 +16,10 @@ public class Student
 
     public void setName(String neuName)
     {
+        /*
+         * Die Eigenschaft name wird überschrieben mit dem Wert des 
+         * Parameter.
+         */
         name = neuName;
     }
 
